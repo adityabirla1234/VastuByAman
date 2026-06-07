d77c2c795d99f7311b1934219d8c8d7541f39ac8
adityabirla1234 <125027789+adityabirla1234@users.noreply.github.com>
2026-06-08 00:05:37 +0530

    Add new routes for KundaliCheck and ConsultationBooking

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PanditAmanBhatore from "./PanditAmanBhatore";
 import VastuVideoGuidancePage from "./VastuVideoGuidancePage";
+import KundaliCheckPage from "./KundaliCheck";
+import ConsultationBookingPage from "./ConsultationBooking";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/"               element={<PanditAmanBhatore />} />
-        <Route path="/video-guidance" element={<VastuVideoGuidancePage />} />
+        <Route path="/"                       element={<PanditAmanBhatore />} />
+        <Route path="/video-guidance"         element={<VastuVideoGuidancePage />} />
+        <Route path="/kundali-check"          element={<KundaliCheckPage />} />
+        <Route path="/consultation-booking"   element={<ConsultationBookingPage />} />
       </Routes>
     </BrowserRouter>
   );
